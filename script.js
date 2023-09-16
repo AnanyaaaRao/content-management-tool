08823bc62c5daee56538fed6f2ac66fa2ef62a8a
AnanyaaaRao <127681528+AnanyaaaRao@users.noreply.github.com>
2023-09-16 22:02:35 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const blogForm = document.getElementById("BlogForm");
+    const titleDisplay = document.getElementById("title-display");
+    const contentDisplay = document.getElementById("content-display");
+    const imageDisplay = document.getElementById("image-display");
+    const videoDisplay = document.getElementById("video-display");
+  
+    blogForm.addEventListener("submit",  (e) => {
+      e.preventDefault();
+  
+      const titleInput = document.getElementById("title").value;
+      const title = titleInput.value;
+      const content = document.getElementById("content").value;
+      const image = document.getElementById("image").files[0];
+      const video = document.getElementById("video").files[0];
+  
+      titleDisplay.textContent = title;
+      contentDisplay.textContent = content;
+      imageDisplay.src = URL.createObjectURL(image);
+      videoDisplay.src = URL.createObjectURL(video);
+  
+      document.getElementById('submitted-content').style.display = 'block';
+      blogForm.reset();
+    });
+  });
